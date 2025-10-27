4f6f98ecf3913fcebc25d1a35e2c8d8ecef88903
Roman-A113 <roman.koles@bk.ru>
2025-10-27 23:52:23 +0500

    add switching between floors

--- app/src/main/java/com/example/studkompas/ui/CampusMapActivity.java
+++ app/src/main/java/com/example/studkompas/ui/CampusMapActivity.java
@@ -10,7 +10,6 @@ import com.example.studkompas.model.CustomPhotoView;
 
 public class CampusMapActivity extends AppCompatActivity {
     private CustomPhotoView photoView;
-    private Button btnToggleLine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +17,33 @@ public class CampusMapActivity extends AppCompatActivity {
         setContentView(R.layout.activity_campus_map);
 
         photoView = findViewById(R.id.imageViewCampusMap);
-        btnToggleLine = findViewById(R.id.btnToggleLine);
-
         photoView.setMaximumScale(10.0f);
-        photoView.postDelayed(() -> photoView.setScale(2.0f, true), 100);
+
+        Button btnFloor1 = findViewById(R.id.btnFloor1);
+        Button btnFloor2 = findViewById(R.id.btnFloor2);
+        Button btnFloor3 = findViewById(R.id.btnFloor3);
+        Button btnFloor4 = findViewById(R.id.btnFloor4);
+        Button btnFloor5 = findViewById(R.id.btnFloor5);
+
+        setupFloorButton(btnFloor1, R.drawable.guk_1);
+        setupFloorButton(btnFloor2, R.drawable.guk_2);
+        setupFloorButton(btnFloor3, R.drawable.guk_3);
+        setupFloorButton(btnFloor4, R.drawable.guk_4);
+        setupFloorButton(btnFloor5, R.drawable.guk_5);
 
 
+        Button btnToggleLine = findViewById(R.id.btnToggleLine);
         btnToggleLine.setOnClickListener(v -> {
-            boolean currentlyShowing = photoView.getShowTestLine();
+           boolean currentlyShowing = photoView.getShowTestLine();
             photoView.setShowTestLine(!currentlyShowing);
             btnToggleLine.setText(currentlyShowing ? "Показать линии" : "Скрыть линии");
+        });
+    }
+
+    private void setupFloorButton(Button button, int drawableResId) {
+        button.setOnClickListener(v -> {
+            photoView.setImageResource(drawableResId);
+            //photoView.postDelayed(() -> photoView.setScale(2.0f, true), 100);
         });
     }
 }
