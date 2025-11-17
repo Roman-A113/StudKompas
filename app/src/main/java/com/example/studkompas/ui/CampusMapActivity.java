15fc3d0bcee87e1a074a94e47f832be25a5364b2
Roman-A113 <roman.koles@bk.ru>
2025-11-17 16:16:03 +0500

    add a smooth transition to the list

--- app/src/main/java/com/example/studkompas/ui/CampusMapActivity.java
+++ app/src/main/java/com/example/studkompas/ui/CampusMapActivity.java
@@ -1,17 +1,25 @@
 package com.example.studkompas.ui;
 
+import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.transition.ChangeBounds;
+import android.transition.Transition;
+import android.transition.TransitionManager;
 import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.DecelerateInterpolator;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -80,31 +88,114 @@ public class CampusMapActivity extends AppCompatActivity {
     }
 
     private void showLocationsList() {
-        if (isLocationsListDisplays)
-            return;
+        if (isLocationsListDisplays) return;
         isLocationsListDisplays = true;
 
-        findViewById(R.id.imageViewCampusMap).animate().alpha(0f).setDuration(200).setInterpolator(new DecelerateInterpolator()).start();
-        findViewById(R.id.editor_controls).animate().alpha(0f).setDuration(200).start();
-        findViewById(R.id.floorPanel).animate().alpha(0f).setDuration(200).start();
+        findViewById(R.id.imageViewCampusMap).setVisibility(View.GONE);
+        findViewById(R.id.editor_controls).setVisibility(View.GONE);
+        findViewById(R.id.floorPanel).setVisibility(View.GONE);
+        findViewById(R.id.MakePathButton).setVisibility(View.GONE);
+
+        if(currentFocusedInputField == findViewById(R.id.editTextStart)){
+            findViewById(R.id.inputLayoutEnd).setVisibility(View.GONE);
+        }else{
+            findViewById(R.id.inputLayoutStart).setVisibility(View.GONE);
+        }
+
+        ConstraintLayout root = findViewById(R.id.mainConstraintLayout);
+        ConstraintSet constraints = new ConstraintSet();
+        constraints.clone(root);
+
+        constraints.clear(R.id.routeInputPanel, ConstraintSet.TOP);
+        constraints.clear(R.id.routeInputPanel, ConstraintSet.BOTTOM);
+        constraints.connect(R.id.routeInputPanel, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, 16);
+        constraints.connect(R.id.routeInputPanel, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START);
+        constraints.connect(R.id.routeInputPanel, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END);
+
+        constraints.clear(R.id.LocationsList, ConstraintSet.TOP);
+        constraints.clear(R.id.LocationsList, ConstraintSet.BOTTOM);
+        constraints.connect(R.id.LocationsList, ConstraintSet.TOP, R.id.routeInputPanel, ConstraintSet.BOTTOM, 8);
+        constraints.connect(R.id.LocationsList, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM);
+        constraints.connect(R.id.LocationsList, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START);
+        constraints.connect(R.id.LocationsList, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END);
+
+        Transition transition = new ChangeBounds();
+        transition.setDuration(150);
+        transition.setInterpolator(new DecelerateInterpolator());
+
+        TransitionManager.beginDelayedTransition(root, transition);
+        constraints.applyTo(root);
 
         locationsList.setVisibility(View.VISIBLE);
-
         List<String> locationNames = GraphManager.getNodeNamesInCampus(selectedCampus.Id);
         locationAdapter.updateList(locationNames);
     }
 
     public void hideLocationsList() {
-        if (!isLocationsListDisplays)
-            return;
+        if (!isLocationsListDisplays) return;
         isLocationsListDisplays = false;
 
-        findViewById(R.id.imageViewCampusMap).animate().alpha(1f).setDuration(200).start();
-        findViewById(R.id.editor_controls).animate().alpha(1f).setDuration(200).start();
-        findViewById(R.id.floorPanel).animate().alpha(1f).setDuration(200).start();
-
         locationsList.setVisibility(View.GONE);
-        currentFocusedInputField.clearFocus();
+
+        if (currentFocusedInputField != null) {
+            currentFocusedInputField.clearFocus();
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(currentFocusedInputField.getWindowToken(), 0);
+            currentFocusedInputField = null;
+        }
+
+        ConstraintLayout root = findViewById(R.id.mainConstraintLayout);
+        ConstraintSet constraints = new ConstraintSet();
+        constraints.clone(root);
+
+        constraints.clear(R.id.routeInputPanel, ConstraintSet.TOP);
+        constraints.clear(R.id.routeInputPanel, ConstraintSet.BOTTOM);
+        constraints.connect(R.id.routeInputPanel, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, 10);
+        constraints.connect(R.id.routeInputPanel, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START);
+        constraints.connect(R.id.routeInputPanel, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END);
+
+        constraints.clear(R.id.LocationsList, ConstraintSet.TOP);
+        constraints.clear(R.id.LocationsList, ConstraintSet.BOTTOM);
+        constraints.connect(R.id.LocationsList, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP);
+        constraints.connect(R.id.LocationsList, ConstraintSet.BOTTOM, R.id.routeInputPanel, ConstraintSet.TOP, 8);
+        constraints.connect(R.id.LocationsList, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START);
+        constraints.connect(R.id.LocationsList, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END);
+        constraints.setVisibility(R.id.LocationsList, ConstraintSet.GONE);
+
+
+        Transition transition = new ChangeBounds();
+        transition.setDuration(150);
+        transition.setInterpolator(new DecelerateInterpolator());
+
+        transition.addListener(new Transition.TransitionListener() {
+            @Override
+            public void onTransitionStart(Transition transition) {}
+
+            @Override
+            public void onTransitionEnd(Transition transition) {
+                findViewById(R.id.imageViewCampusMap).setVisibility(View.VISIBLE);
+                findViewById(R.id.editor_controls).setVisibility(View.VISIBLE);
+                findViewById(R.id.floorPanel).setVisibility(View.VISIBLE);
+                findViewById(R.id.inputLayoutStart).setVisibility(View.VISIBLE);
+                findViewById(R.id.inputLayoutEnd).setVisibility(View.VISIBLE);
+                findViewById(R.id.MakePathButton).setVisibility(View.VISIBLE);
+
+            }
+
+            @Override
+            public void onTransitionCancel(Transition transition) {
+                onTransitionEnd(transition);
+            }
+
+            @Override
+            public void onTransitionPause(Transition transition) {}
+
+            @Override
+            public void onTransitionResume(Transition transition) {}
+        });
+
+        TransitionManager.beginDelayedTransition(root, transition);
+        constraints.applyTo(root);
     }
 
     private void switchToFloor(int floorNumber) {
